184cff5a4248b2740705d6544bd60709627ed683
tajk19 <107914392+tajk19@users.noreply.github.com>
2026-09-13 16:07:12 +0300

    Add models/flashcard.ts

---
+++ models/flashcard.ts
@@ -0,0 +1,22 @@
+export interface Flashcard {
+  question: string;
+  answer: string;
+}
+
+export interface GeneratedFlashcard extends Flashcard {
+  evidence: string;
+}
+
+export interface FlashcardResponse {
+  cards: GeneratedFlashcard[];
+}
+
+export interface GenerateFlashcardsOptions {
+  apiKey: string;
+  model: string;
+  note: string;
+  count: number;
+  instructions: string;
+  signal?: AbortSignal;
+}
+
