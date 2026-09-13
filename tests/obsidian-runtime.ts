c088a05ee5f5a20c14aa22052adf343bc97c6a81
tajk19 <107914392+tajk19@users.noreply.github.com>
2026-09-13 16:03:55 +0300

    Add tests/obsidian-runtime.ts

---
+++ tests/obsidian-runtime.ts
@@ -0,0 +1,18 @@
+export class TFile {
+  path = "";
+}
+
+export class TFolder {
+  path = "";
+}
+
+export function normalizePath(value: string): string {
+  return value.replace(/\\/g, "/").replace(/\/{2,}/g, "/");
+}
+
+export function requestUrl(): Promise<never> {
+  return Promise.reject(
+    new Error("Obsidian requestUrl must be mocked in unit tests.")
+  );
+}
+
